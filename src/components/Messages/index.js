628ac720c2026d5080e6bafe2f3b1b36c813d70f
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-10-13 15:34:44 +0200

    feat : composant Messages

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Message from 'src/components/Message';
 
 function Messages(props) {
+
+    const messages= useSelector((state) => state.messages );
+
     return (
-        <div>
-        <Message /> 
+        <div className='messages'>
+        {
+            messages.map( (message) => (
+                <div
+                key={message.id}
+                className="message">
+                    <div>{message.author}</div>
+                    <div>{message.content}</div>
+                </div>
+             )
+            )
+        }
         </div>
     );
 }
