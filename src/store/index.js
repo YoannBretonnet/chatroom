f41ee1aefec03eeada6c95bfd898b432876d8540
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-10-13 15:22:01 +0200

    feat: initialState

--- src/store/index.js
+++ src/store/index.js
@@ -1,2 +1,18 @@
 import { createStore } from 'redux';
 
+const initialState = {
+    messages : [
+    {
+        id: 1,
+        author: "Yoyo",
+        content: "Salut!",
+    },
+    {
+        id: 2,
+        author: "Yoyo",
+        content: "Tu fais quoi ce soir?",
+    }
+    ],
+    inputMessage : '',
+};
+
